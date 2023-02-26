39ef13ad239b323968becfb63dff5b8625c09d9d
abhisek-dhua <101027155+abhisek-dhua@users.noreply.github.com>
2023-02-26 22:14:40 +0530

    reactive form

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,59 @@
 import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
+    <h1>Hello im {{name}}!</h1>
+    <form [formGroup]=form (ngSubmit)="onSubmit()">
+    <div class="mb-3 row">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control-plaintext" id="staticEmail" placeholder="email@example.com" formControlName="email">
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword" formControlName="password">
+    </div>
+  </div>
+  <div class="col-auto">
+    <button type="submit" class="btn btn-primary mb-3">Confirm identity</button>
+  </div>
+    </form>
   `,
 })
-export class App {
-  name = 'Angular';
+export class App implements OnInit {
+  name = 'Abhisk';
+  form!: FormGroup;
+  initialValue!: any;
+
+  ngOnInit(): void {
+    this.initilizeForm();
+  }
+
+  initilizeForm() {
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
+    this.initialValue = this.form.getRawValue();
+  }
+
+  onSubmit() {
+    console.log(this.form.getRawValue());
+  }
 }
 
 bootstrapApplication(App);
